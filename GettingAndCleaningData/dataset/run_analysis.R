6f4ad57f81b01ebbcecf65eece1e8983470692fe
dinoamaral <dino@Dinos-MacBook-Air.local>
2015-07-26 18:22:02 -0300

    Course Project

--- GettingAndCleaningData/dataset/run_analysis.R
+++ GettingAndCleaningData/dataset/run_analysis.R
@@ -1,30 +1,30 @@
 ####You should create one R script called run_analysis.R that does the following. 
 # 1.Merges the training and the test sets to create one data set.
 # setwd("~/Dropbox/Coursera/datasciencecoursera/GettingAndCleaningData/data")
-trainData <- read.table("./dataset/train/X_train.txt")
-trainRating <- read.table("./dataset/train/y_train.txt")
-trainSubject <- read.table("./dataset/train/subject_train.txt")
+trainData <- read.table("./data/train/X_train.txt")
+trainRating <- read.table("./data/train/y_train.txt")
+trainSubject <- read.table("./data/train/subject_train.txt")
 
-testData <- read.table("./dataset/test/X_test.txt")
-testRating <- read.table("./dataset/test/y_test.txt")  
-testSubject <- read.table("./dataset/test/subject_test.txt")
+testData <- read.table("./data/test/X_test.txt")
+testRating <- read.table("./data/test/y_test.txt")  
+testSubject <- read.table("./data/test/subject_test.txt")
 
 joinData <- rbind(trainData, testData)
 joinLabel <- rbind(trainRating, testRating)
 joinSubject <- rbind(trainSubject, testSubject)
 
 # 2.Extracts only the measurements on the mean and standard deviation for each measurement. 
-measures <- read.table("./dataset/features.txt")
+measures <- read.table("./data/features.txt")
 mean_std <- grep("mean\\(\\)|std\\(\\)", measures[, 2])
  
 joinData <- joinData[, mean_std]
-names(joinData) <- gsub("\\(\\)", "", measures[mean_std, 2]) # remove "()"
-names(joinData) <- gsub("mean", "Mean", names(joinData)) # capitalize M
-names(joinData) <- gsub("std", "Std", names(joinData)) # capitalize S
-names(joinData) <- gsub("-", "", names(joinData)) # remove "-" in column names 
+names(joinData) <- gsub("\\(\\)", "", measures[mean_std, 2])
+names(joinData) <- gsub("mean", "Mean", names(joinData))
+names(joinData) <- gsub("std", "Std", names(joinData)) 
+names(joinData) <- gsub("-", "", names(joinData)) 
 
 # 3. Uses descriptive activity names to name the activities in the data set
-activity_label <- read.table("./dataset/activity_labels.txt")
+activity_label <- read.table("./data/activity_labels.txt")
 activity_label[, 2] <- tolower(gsub("_", "", activity_label[, 2]))
 substr(activity_label[2, 2], 8, 8) <- toupper(substr(activity_label[2, 2], 8, 8))
 substr(activity_label[3, 2], 8, 8) <- toupper(substr(activity_label[3, 2], 8, 8))
@@ -35,7 +35,7 @@ names(joinLabel) <- "activity"
 # 4. Appropriately labels the data set with descriptive activity names. 
 names(joinSubject) <- "subject"
 cjoined_data <- cbind(joinSubject, joinLabel, joinData)
-write.table(cjoined_data, "~/GitHub/DataScienceCoursera/GettingAndCleaningData/data/merged_data.txt")
+write.table(cjoined_data, "~/GitHub/DataScienceCoursera/GettingAndCleaningData/dataset/merged_data.txt")
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each 
 # variable for each activity and each subject.
@@ -58,5 +58,4 @@ for(i in 1:subject_Len) {
     nrow <- nrow + 1
   }
 }
-head(result)
-write.table(result, "~/GitHub/DataScienceCoursera/GettingAndCleaningData/data/data_with_means.txt")
+write.table(result, "~/GitHub/DataScienceCoursera/GettingAndCleaningData/dataset/data_with_means.txt")
